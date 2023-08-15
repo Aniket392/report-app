f129d76aa5dca8dbbeaabf69534c4f1436c8fec7
Aniket <ani24866@gmail.com>
2023-08-15 20:12:30 +0530

    debug update

--- reports_proj/settings.py
+++ reports_proj/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 ADMIN_URL = os.environ.get('ADMIN_URL')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
